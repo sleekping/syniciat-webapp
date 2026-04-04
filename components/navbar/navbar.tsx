3d8f87fce933a9b8ab71a393264507259b739f31
ayindequdus22 <ayindeabdulqudus22@gmail.com>
2026-04-04 15:32:22 -0700

    refactor: navbar responsiveness

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -6,6 +6,7 @@ import Image from "next/image"
 import { ActiveLink } from "../ui/active-link";
 import { IoChevronDown } from "react-icons/io5";
 import { IoMenu } from "react-icons/io5";
+import { LiaTimesSolid } from "react-icons/lia";
 type NavLinks = (
     | {
         title: string;
@@ -82,7 +83,9 @@ const links: NavLinks = [
 ]
 
 export function Navbar() {
+    // for the scroll effect
     const [activeScroll, setActiveScroll] = useState(false);
+    // for the mobile menu
     const [menuOpen, setMenuOpen] = useState(false);
     console.log(menuOpen)
     useEffect(() => {
@@ -110,11 +113,13 @@ export function Navbar() {
             </Link>
 
             {/* links */}
-            <div className={`linkOpacityContainer  max-mdLap:bg-[rgba(0,0,0,.5)]` + (menuOpen ? " translate-x-0" : "")
+            <div className={`linkOpacityContainer max-mdLap:bg-[rgba(0,0,0,.5)]` + (menuOpen ? " open" : "")
             }>
-
+                {/*+  (menuOpen ? " translate-x-0" : "translate-x-200") */}
 
                 <div className={"gap-10 flex linkContainer"}>
+
+                    <LiaTimesSolid className={`cursor-pointer text-3xl place-self-end mt-10 hidden max-mdLap:block mb-10 text-gray-500 hover:text-primary transition-colors duration-200`} onClick={() => setMenuOpen(false)} />
 
                     {links.map((link, id) => (
                         <div key={id}>
@@ -126,18 +131,18 @@ export function Navbar() {
                                 "type" in link && link.type === "product" && (
                                     <div className={`dropdown-container group ${openDropdown === id ? "open" : ""
                                         }`}>
-                                        <button className="flex items-center gap-1 link" onClick={() =>
+                                        <button className="flex items-center gap-1 link max-mdLap:justify-between max-mdLap:w-full" onClick={() =>
                                             setOpenDropdown(openDropdown === id ? null : id)
                                         }>{link.title} <IoChevronDown className="icon" /></button>
                                         <div className="dropdown">
-                                            <p className="border-b font-semibold text-2xl border-gray-300 pb-4">All products</p>
-                                            <div className="flex flex-row gap-36 pt-4 max-mdLap:flex-col max-mdLap:gap-4">
+                                            <p className="mdLap:border-b font-semibold text-2xl border-gray-300 pb-4 max-mdLap:pl-2 max-mdLap:py-2">All products</p>
+                                            <div className="flex flex-row gap-36 pt-4 max-mdLap:flex-col max-mdLap:gap-4 max-mdLap:pt-1 ">
                                                 {link.dropdownItem.map((v, i) => (
                                                     <div className="" key={i}>
-                                                        <p className="text-primary font-medium text-2xl">{v.heading}</p>
+                                                        <p className="text-primary font-medium text-2xl max-mdLap:pl-4">{v.heading}</p>
                                                         <div className="flex flex-col gap-7 pt-6">
                                                             {v.product.map((p, j) => (
-                                                                <Link href={p.href} key={j} className="text-2xl font-medium  text-gray-500  hover:text-primary transition-colors duration-100 ease-in-out">{p.title}</Link>
+                                                                <Link href={p.href} key={j} className="max-mdLap:pl-8 text-2xl font-medium  text-gray-500  hover:text-primary transition-colors duration-100 ease-in-out">{p.title}</Link>
                                                             ))}
                                                         </div>
                                                     </div>
@@ -154,13 +159,13 @@ export function Navbar() {
                                 "type" in link && link.type === "project" && (
                                     <div className={`dropdown-container group ${openDropdown === id ? "open" : ""
                                         }`}>
-                                        <button className="flex items-center gap-1 link" onClick={() =>
+                                        <button className="flex items-center gap-1 link max-mdLap:justify-between max-mdLap:w-full" onClick={() =>
                                             setOpenDropdown(openDropdown === id ? null : id)
                                         }>{link.title} <IoChevronDown className="icon" /></button>
                                         <div className="dropdown ">
                                             <h6 className="text-4xl text-primary font-bold max-mdLap:hidden">{link.dropdownData.heading}</h6>
                                             <p className="text-gray-500 pt-4 font-medium text-xl  max-mdLap:hidden">{link.dropdownData.subHeading}</p>
-                                            <div className="flex flex-row  max-mdLap:flex-col max-mdLap:gap-4 gap-36 pt-4">
+                                            <div className="flex flex-row  max-mdLap:flex-col max-mdLap:gap-4 gap-36 pt-4 max-mdLap:pl-8 max-mdLap:pt-0">
                                                 {link.dropdownData.projects.map((v, i) => (
                                                     <Link href={{ pathname: "/projects", query: { city: v.title } }} key={i} className="flex flex-col gap-7 pt-6">
                                                         {/* <Image src={v.image} alt={v.title} width={100} height={100} className="w-24 h-24" />  */}
