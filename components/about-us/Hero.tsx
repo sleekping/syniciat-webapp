0419687775504ff3a29a4e5a232923992577c3a3
ayindequdus22 <ayindeabdulqudus22@gmail.com>
2026-04-14 11:32:25 -0700

    style: made the hero section responsive

--- components/about-us/Hero.tsx
+++ components/about-us/Hero.tsx
@@ -2,16 +2,14 @@ import React from 'react'
 
 const Hero = (): React.JSX.Element => {
     return (
-        <section className="svg-container px-4 h-[70vh] flex flex-col items-center text-center space-y-5 justify-center gap-4">
-            <h1 className='text-7xl font-bold text-white'>About Syniciat Energy & Tech Solutions
+        <section className="svg-container px-8 max-tab:px-4 h-[70vh] flex flex-col items-center text-center space-y-5 justify-center gap-4">
+            <h1 className='text-7xl max-mdLap:text-6xl max-tab:text-5xl max-mdPhone:text-4xl font-bold text-white'>About Syniciat Energy & Tech Solutions
             </h1>
-            <p className=' text-blue-50 text-4xl leading-relaxed max-w-5xl'>Innovative Energy and Security Solutions Designed for Efficiency Safety and Peace of Mind</p>
+            <p className=' text-blue-50 text-4xl leading-relaxed max-w-5xl max-tab:max-w-full max-tab:text-3xl max-mdPhone:text-2xl'>Innovative Energy and Security Solutions Designed for Efficiency Safety and Peace of Mind</p>
             <div className="animateContainer">
-
                 <div className="animateItem"></div>
             </div>
 
         </section>)
 }
-// w-35 h-1.5 bg-white rounded-full 
 export default Hero
